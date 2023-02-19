2775f20022ea45ed0f3f43f9ee864da0734a0a21
Abhay-codes <abhaysingh002001@gmail.com>
2023-02-20 02:28:13 +0530

    distinct numbers

---
+++ Searching and sorting/distinct nubers(set).py
@@ -0,0 +1,44 @@
+import heapq 
+from collections import defaultdict 
+import math 
+import collections
+
+
+
+mod=10**9+7
+alp='#abcdefghijklmnopqrstuvwxyz'
+cap='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+digit='1234567890'
+
+
+
+#math.facorial(n)------>gives the factorial 
+
+
+
+
+
+
+for _ in range(1):
+    
+    n =int(input())
+    #n,k =map(int,input().split())
+    '''l=[]
+    for   i in range(n):
+        l1=list(map(int,input().split()))
+        l.append(l1)'''
+    #print(math.gcd(-2,-6))
+    l=list(map(int,input().split()))
+    
+    #l2=list(map(int,input().split()))
+    #print(l)
+    #print(n)
+    s=set(l)
+    print(len(s))
+    
+        
+    
+    
+    
+
+
